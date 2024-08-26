27721fecd7ac2fc95d2dccdb4c6ee9fcb6339f67
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-25 21:50:47 -0400

    feat: Update web scraper to handle HTTP errors and display error messages

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+require 'uri'
 
 url = ARGV[0] || gets.chomp
 
-html = URI.open(url)
+begin
+  html = URI.open(url, "User-Agent" => "Mozilla/5.0")
+  doc = Nokogiri::HTML(html)
 
-doc = Nokogiri::HTML(html)
+  links = doc.css('a')
+  filtered_links = links.select { |link| link['href'] =~ /^http/ }
 
-links = doc.css('a')
-
-filtered_links = links.select { |link| link['href'] =~ /^http/ }
-
-CSV.open('links.csv', 'wb') do |csv|
+  CSV.open('links.csv', 'wb') do |csv|
     csv << ['Index', 'Title', 'Link']
     filtered_links.each_with_index do |link, index|
       title = link.text.strip
@@ -21,4 +21,10 @@ CSV.open('links.csv', 'wb') do |csv|
     end
   end
 
-puts "Links saved to links.csv"
+  puts "Links saved to links.csv"
+
+rescue OpenURI::HTTPError => e
+  puts "HTTP Error: #{e.message}"
+rescue StandardError => e
+  puts "An error occurred: #{e.message}"
+end
