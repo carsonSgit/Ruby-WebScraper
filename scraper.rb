23f139e2348486e09474c2acb84f0bfe4a481498
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-25 21:47:34 -0400

    feat: Scraper gets index, title, and link information

--- scraper.rb
+++ scraper.rb
@@ -13,7 +13,9 @@ links = doc.css('a')
 filtered_links = links.select { |link| link['href'] =~ /^http/ }
 
 CSV.open('links.csv', 'wb') do |csv|
+    csv << ['Index', 'Title', 'Link']
     filtered_links.each_with_index do |link, index|
-        csv << [index + 1, link['href']]
+      title = link.text.strip
+      csv << [index + 1, title, link['href']]
     end
-end
+  end
